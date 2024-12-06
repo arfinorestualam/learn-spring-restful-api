4fea6969e54cdec058d1a204224527ae6a59cdda
arfinorestualam <arfino.alam@gmail.com>
2024-12-06 07:44:39 +0700

    make user entity

--- database.sql
+++ database.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE belajar_spring_restful_api;
 
 USE belajar_spring_restful_api;
+
+CREATE TABLE users
+(
+    username            varchar(100)    NOT NULL ,
+    password            varchar(100)    NOT NULL ,
+    name                varchar(100)    NOT NULL ,
+    token               varchar(100),
+    token_expired_at    BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+) ENGINE InnoDB;
+
+SELECT * FROM users;
