1929ce1485571ad90a96046f5a4082bca8283d68
arfinorestualam <arfino.alam@gmail.com>
2024-12-06 12:45:38 +0700

    make contact entity

--- database.sql
+++ database.sql
@@ -13,4 +13,16 @@ CREATE TABLE users
     UNIQUE (token)
 ) ENGINE InnoDB;
 
-SELECT * FROM users;
+SELECT * FROM contacts;
+
+CREATE TABLE contacts
+(
+    id          VARCHAR(100)    NOT NULL ,
+    username    VARCHAR(100)    NOT NULL ,
+    first_name  VARCHAR(100)    NOT NULL ,
+    last_name   VARCHAR(100),
+    phone       VARCHAR(100),
+    email       VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_users_contacts (username) REFERENCES users (username)
+) ENGINE InnoDB;
---
+++ src/main/java/pzn/restful/entity/Contact.java
@@ -0,0 +1,31 @@
+package pzn.restful.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+}
