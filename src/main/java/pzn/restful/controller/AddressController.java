cc98a707401f2f9f49beb1a946b3251b8e7e6256
arfinorestualam <arfino.alam@gmail.com>
2024-12-15 15:12:07 +0700

    make update address api

--- src/main/java/pzn/restful/controller/AddressController.java
+++ src/main/java/pzn/restful/controller/AddressController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import pzn.restful.entity.User;
 import pzn.restful.model.AddressResponse;
 import pzn.restful.model.CreateAddressRequest;
+import pzn.restful.model.UpdateAddressRequest;
 import pzn.restful.model.WebResponse;
 import pzn.restful.service.AddressService;
 
@@ -39,5 +40,23 @@ public class AddressController {
                                             @PathVariable("addressId") String addressId) {
         AddressResponse response = addressService.get(user, contactId, addressId);
         return WebResponse.<AddressResponse>builder().data(response).build();
+    }
+
+    @PutMapping(
+            path = "/api/contacts/{contactId}/addresses/{addressId}",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<AddressResponse> update(User user,
+                                               @RequestBody UpdateAddressRequest request,
+                                               @PathVariable("contactId") String contactId,
+                                               @PathVariable("addressId") String addressId) {
+        request.setContactId(contactId);
+        request.setAddressId(addressId);
+
+        AddressResponse response = addressService.update(user, request);
+        return WebResponse.<AddressResponse>builder()
+                .data(response)
+                .build();
     }
 }
